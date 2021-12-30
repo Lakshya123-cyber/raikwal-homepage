d956a8b72bee9bb85afd57acd68592ed56279ee8
Lakshya Raikwal <lakshya.raikwal@giis.edu.sg>
2021-12-30 21:54:11 +0800

    Update snake.js

--- pages/projects/snake.js
+++ pages/projects/snake.js
@@ -24,7 +24,7 @@ const Work = () => (
         </ListItem>
       </List>
 
-      <WorkImage src="/images/works/snek.gif" alt="Snek" />
+      <WorkImage src="/images/works/snek.gif" alt="Snake" />
     </Container>
   </Layout>
 )
