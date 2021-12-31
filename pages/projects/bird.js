d11b5d9e7b3592bdbcdb1929464d5bf819693f3d
Lakshya Raikwal <lakshya.raikwal@giis.edu.sg>
2021-12-31 23:04:25 +0800

    Update bird.js

--- pages/projects/bird.js
+++ pages/projects/bird.js
@@ -1,4 +1,5 @@
-import { Container, Badge, List, ListItem } from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../../components/projects'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
@@ -15,6 +16,12 @@ const Work = () => (
       </P>
       <List ml={4} my={4}>
         <ListItem>
+          <Meta>Source</Meta>
+          <Link href="https://github.com/Lakshya123-cyber/AI_FlappyGame">
+            Neural Bird <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
           <Meta>Platform</Meta>
           <span>Windows/macOS</span>
         </ListItem>
@@ -24,7 +31,6 @@ const Work = () => (
         </ListItem>
       </List>
 
-      <WorkImage src="/images/works/AI_BIRD.gif" alt="Bird" />
       <WorkImage src="/images/works/bird.png" alt="Bird" />
     </Container>
   </Layout>
