92dc19c776e658a601e94f79bc48693e4b45592e
Lakshya Raikwal <lakshya.raikwal@giis.edu.sg>
2021-12-31 23:22:24 +0800

    Update wpm.js

--- pages/projects/wpm.js
+++ pages/projects/wpm.js
@@ -1,4 +1,5 @@
-import { Container, Badge, List, ListItem } from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../../components/projects'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
@@ -14,6 +15,12 @@ const Work = () => (
         are on your keyboard.
       </P>
       <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Source</Meta>
+          <Link href="https://github.com/Lakshya123-cyber/WPM-Tester">
+            WPM Tester <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
         <ListItem>
           <Meta>Platform</Meta>
           <span>Windows/macOS</span>
