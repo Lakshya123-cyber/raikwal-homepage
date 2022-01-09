32bc2763a706c4e582f088e62afcb0620e4273b6
Lakshya Raikwal <lakshya.raikwal@giis.edu.sg>
2022-01-09 16:38:53 +0800

    Update blink.js

--- pages/projects/blink.js
+++ pages/projects/blink.js
@@ -1,4 +1,5 @@
-import { Container, Badge, List, ListItem } from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../../components/projects'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
@@ -9,8 +10,16 @@ const Work = () => (
       <Title>
         Blink Counter <Badge>2021</Badge>
       </Title>
-      <P>An app which can count the number of Blinks.</P>
+      <P>
+        An app which can count the number of blinks.
+      </P>
       <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Source</Meta>
+          <Link href="https://github.com/Lakshya123-cyber/Blink-Counter">
+            Blink Counter <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
         <ListItem>
           <Meta>Platform</Meta>
           <span>Windows/macOS</span>
@@ -21,8 +30,8 @@ const Work = () => (
         </ListItem>
       </List>
 
-      <WorkImage src="/images/works/blink.PNG" alt="Bird" />
-      <WorkImage src="/images/works/blink1.PNG" alt="Bird" />
+      <WorkImage src="/images/works/blink.PNG" alt="Blink" />
+        <WorkImage src="/images/works/blink1.PNG" alt="Blink" />
     </Container>
   </Layout>
 )
