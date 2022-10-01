74676aeaad9627df0a02f345440aaa710521fe49
Lakshya Raikwal <lakshya.raikwal@giis.edu.sg>
2022-10-01 14:42:40 +0800

    udpated the style of writing my name in the logo

--- components/logo.js
+++ components/logo.js
@@ -31,7 +31,7 @@ const Logo = () => {
             fontWeight="bold"
             ml={3}
           >
-            Lakshya Raikwal
+            LakshyaRaik
           </Text>
         </LogoBox>
       </a>
