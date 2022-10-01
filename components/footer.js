0aaf8f80bc64efa8772b7676e7b54d36c4efa0dc
Lakshya Raikwal <lakshya.raikwal@giis.edu.sg>
2022-10-01 14:43:13 +0800

    Updated the style of writing my name in the footer

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} Lakshya Raikwal. All Rights Reserved.
+      &copy; {new Date().getFullYear()} LakshyaRaik. All Rights Reserved.
     </Box>
   )
 }
